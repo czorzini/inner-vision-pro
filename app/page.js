8c2cce7e7b01a9428cbd2ba199c38253235dfb24
fahim2204 <fahimfaisal1998@gmail.com>
2024-02-18 23:01:00 +0600

    Fix Vercel Error

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import DiscoverSection from '@/components/DiscoverSection'
-import FooterSection from '@/components/FooterSection'
-import HeaderSection from '@/components/HeaderSection'
-import HeroSection from '@/components/HeroSection'
-import IntroducingSection from '@/components/IntroducingSection'
-import ReDiscoverSection from '@/components/ReDiscoverSection'
-import TuneRealitySection from '@/components/TuneRealitySection'
+import DiscoverSection from '../components/DiscoverSection'
+import FooterSection from '../components/FooterSection'
+import HeaderSection from '../components/HeaderSection'
+import HeroSection from '../components/HeroSection'
+import IntroducingSection from '../components/IntroducingSection'
+import ReDiscoverSection from '../components/ReDiscoverSection'
+import TuneRealitySection from '../components/TuneRealitySection'
 import Image from 'next/image'
 
 export default function Home() {
